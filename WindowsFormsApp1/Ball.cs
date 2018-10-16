059bb149ab6b1b5e36a2c8d32aa8db2c2ac60f5b
shoppai-shoochan <shoppai.shoochan@gmail.com>
2018-10-17 00:51:36 +0900

    first commit

---
+++ WindowsFormsApp1/Ball.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+    class Ball
+    {
+        private int diam;//ボールの直径
+        private int x, y;//X座標、Y座標
+        private int vx, vy;//ボールの速度
+        private int pre_x, pre_y;//１つ前のX座標、Y座標
+        private Bitmap canvas;
+        private Brush color;
+        private bool clicked;//クリック判定フラグ
+        static public int ballcount = -1;//金ボールの総数、見本用に１つ多く生成するため-1からスタート
+        static Random rnd = new System.Random();//乱数生成オブジェクト
+
+        //コンストラクタ
+        public Ball(int diam, int x, int y, int v, Bitmap canvas, Brush color)
+        {
+            this.x = x;
+            this.y = y;
+            this.vx = v;
+            this.vy = v;
+            this.diam = diam;
+            this.color = color;
+            this.canvas = canvas;
+            clicked = false;
+            if (color == Brushes.Gold) { ++ballcount; }//金のボールを生成したらカウントアップ
+        }
+        //パラメータ初期化
+        public void Ini(int diam, int x, int y, int v, Bitmap canvas, Brush color)
+        {
+            this.x = x;
+            this.y = y;
+            this.vx = v;
+            this.vy = v;
+            this.diam = diam;
+            this.color = color;
+            this.canvas = canvas;
+            clicked = false;
+            if (color == Brushes.Gold) { ++ballcount; }//金のボールを生成したらカウントアップ
+        }
+        //ボールを描画
+        public Bitmap DrawCircle()
+        {
+            if (!clicked)//まだクリックされてなければ
+            {
+                using (Graphics g = Graphics.FromImage(canvas))
+                {
+                    g.FillEllipse(color, x, y, diam, diam);//ボールを描画
+                }
+                pre_x = x;
+                pre_y = y;
+            }
+            return canvas;
+        }
+        //ボールを消去
+        public void EraseCircle()
+        {
+            if (!clicked)//まだクリックされていなければ
+            {
+                using (Graphics g = Graphics.FromImage(canvas))
+                {
+                    g.FillEllipse(Brushes.White, pre_x, pre_y, diam, diam);//白で塗ってボールを消去
+                }
+            }
+        }
+        //ボール移動
+        public Bitmap Move()
+        {
+            x = x + vx;//座標更新
+            y = y + vy;//座標更新
+            ColBox();//ゲーム画面４辺の衝突処理
+            EraseCircle();//過去座標のボールを消去
+            return DrawCircle();//現在座標にボールを描画
+        }
+        //ゲーム画面４辺の衝突処理
+        public void ColBox()
+        {
+            if (x >= canvas.Width - diam)//右辺の当たり判定
+            {
+                x = canvas.Width - diam - 1;//ｘ座標を右辺から直径分差し引いた値に設定
+                vx *= -1;//速度反転
+            }
+            if (x <= 0)//左辺の当たり判定
+            {
+                x = 0;
+                vx *= -1;
+            }
+            if (y >= canvas.Height - diam)//下辺の当たり判定
+            {
+                y = canvas.Height - diam - 1;//y座標を下辺から直径分差し引いた値に設定
+                vy *= -1;
+            }
+            if (y <= 0)//上辺の当たり判定
+            {
+                y = 0;
+                vy *= -1;
+            }
+        }
+        //ボール同士の衝突処理
+        public void ColBall(Ball otherball)
+        {
+            int[] self_entity;//自身の実体領域
+            int[] other_entity;//相手の実体領域
+            bool[] col = { false, false, false, false };
+            self_entity = Entity(); //自身の実体を四角形とし、座標値をtop,under,left,rightの順に格納
+            other_entity = otherball.Entity();//相手の実体を四角形とし、座標値をtop,under,left,rightの順に格納
+            if (!clicked && !otherball.clicked)//自身も相手もまだクリックされていなければ
+            {
+                //上辺の当たり判定
+                col[0] = self_entity[0] > other_entity[0] && self_entity[0] < other_entity[1];
+                //下辺の当たり判定
+                col[1] = self_entity[1] < other_entity[1] && self_entity[1] > other_entity[0];
+                //左辺の当たり判定
+                col[2] = self_entity[2] > other_entity[2] && self_entity[2] < other_entity[3];
+                //右辺の当たり判定
+                col[3] = self_entity[3] < other_entity[3] && self_entity[3] > other_entity[2];
+                if ((col[0] || col[1]) && (col[2] || col[3]))//x方向とｙ方向どちらも衝突していたらボールが衝突
+                {
+                    ColisionX(otherball);//X方向の衝突処理
+                    ColisionY(otherball);//Y方向の衝突処理
+                }
+            }
+        }
+        //自身の実体を四角形とし、座標値をtop,under,left,rightの順に格納
+        private int[] Entity()
+        {
+            int[] entity;
+            entity = new int[4];
+            int pad;
+            pad = diam / 10;           
+            entity[0] = y + pad; //top            
+            entity[1] = y + diam - pad;//under            
+            entity[2] = x + pad;//left            
+            entity[3] = x + diam - pad;//right
+            return entity;
+        }
+        //衝突時の処理（Y軸）
+        private void ColisionY(Ball otherball)
+        {
+            int pad;
+            pad = 0;
+            if (vy * otherball.vy < 0)//逆向きに進んでいた時に衝突したら
+            {
+                vy *= -1;
+                otherball.vy *= -1;
+                y = y + pad / 2 * vy;//速度とパッドの割合だけずらす
+                otherball.y = otherball.y + pad * otherball.vy / 2 * otherball.vy;//速度とパッドの割合だけずらす
+            }
+            else
+            {
+                if (vy >= otherball.vy)//同じ向きに進んでいて、かつ、自身の速度の方が速い
+                {
+                    vy *= -1;
+                    y = y + pad / 2 * vy;
+                    otherball.y = otherball.y + pad / 2 * otherball.vy;//速度とパッドの割合だけずらす
+                }
+                else//同じ向きに進んでいて、かつ、自身の速度の方が遅い
+                {
+                    otherball.vy *= -1;
+                    y = y + pad / 2 * vy;
+                    otherball.y = otherball.y + pad / 2 * otherball.vy;//速度とパッドの割合だけずらす
+                }
+            }
+        }
+        //衝突時の処理（X軸)
+        private void ColisionX(Ball otherball)
+        {
+            int pad;
+            pad = 10;
+            if (vx * otherball.vx < 0)//逆向きに進んでいた時に衝突したら
+            {
+                vx *= -1;
+                otherball.vx *= -1;
+                x = x + pad / 2 * vx;//速度とパッドの割合だけずらす
+                otherball.x = otherball.x + pad / 2 * otherball.vx;//速度とパッドの割合だけずらす
+            }
+            else
+            {
+                if (vx >= otherball.vx)//同じ向きに進んでいて、かつ、自身の速度の方が速い
+                {
+                    vx *= -1;
+                    x = x + diam / 2 * vx;
+                    otherball.x = otherball.x + pad / 2 * otherball.vx;//速度とパッドの割合だけずらす
+                }
+                else//同じ向きに進んでいて、かつ、自身の速度の方が遅い
+                {
+                    otherball.vx *= -1;
+                    x = x + diam / 2 * vx;
+                    otherball.x = otherball.x + pad / 2 * otherball.vx;//速度とパッドの割合だけずらす
+                }
+            }
+        }
+        //クリック判定
+        public void Check_Clicked(int x, int y)
+        {
+            int[] entity = Entity();//自身の実体を四角形とし、座標値をtop,under,left,rightの順に格納
+            if (!clicked)//クリックされていなければ
+            {
+                if (entity[0] < y && entity[1] > y && entity[2] < x && entity[3] > x)//クリック座標が実体の内側にあれば
+                {
+                    EraseCircle();//ボールを消去
+                    clicked = true;
+                    if (color == Brushes.Gold) { --ballcount; }//金のボールであればカウントダウン
+                }
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FormBallGame : Form
+    {
+        private Ball[] balls;//ボールインスタンスの配列を格納
+        private Ball selectball;//セレクトpictureboxに描画するボール
+        private Brush[] colors = {Brushes.Green, Brushes.Black, Brushes.Goldenrod, Brushes.Pink, Brushes.Red,Brushes.Gold };//ボールの色の種類
+        Bitmap selectCanvas;
+        Bitmap mainCanvas;
+        int num_gold = 5;//金のボールの数
+        int num = 30;//ボールの数
+        int num_colors = 5;//カラーの総数（ゴールドを除く)
+        int index_gold = 5;//colors配列におけるゴールドのindex
+        int ballsize_min = 15, ballseize_max = 50;//作成するボールサイズの範囲
+        int v_min= -10, v_max = 10;//作成するボール速度の範囲
+        bool game_flag = false;//ゲームフラグ、ゲーム中はtrue
+        Random rnd = new System.Random();//乱数生成オブジェクト
+
+        //コンストラクタ
+        public FormBallGame()    
+        {
+            InitializeComponent();
+        }       
+        //ゲームの準備
+        private void FormBallGame_Load(object sender, EventArgs e)
+        {
+            balls = new Ball[num];        
+            selectCanvas =  new Bitmap(selectPictureBox.Width, selectPictureBox.Height);
+            mainCanvas =   new Bitmap(mainPictureBox.Width, mainPictureBox.Height);
+            selectball = new Ball(selectPictureBox.Height, 0,0,0, selectCanvas, Brushes.Gold);//selectpictureboxに描画するボールを生成
+            selectPictureBox.Image = selectball.DrawCircle();//selectpictureboxにボールを描画
+            make_Main_Balls(0, num_gold, num_colors);//mainpictureboxに描画するボール（金）を生成
+            make_Main_Balls(num_gold, num, 0);//mainpictureboxに描画するボールを生成
+            label2.Text = Ball.ballcount.ToString();//金のボールの数を表示
+        }
+        //ゲームスタート
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+            if (game_flag == false)//ゲーム中でなければ
+            {
+                timer1.Start();//タイマー起動
+                restartButton.Text = "STOP";
+                game_flag = true;
+            }
+            else//ゲーム中であれば
+            {
+                reset_Game();//ゲーム停止、リセット
+            }
+        }
+        //ゲーム終了、リセット  
+        private void reset_Game()
+        {
+            int i;
+            Ball.ballcount = 0;
+            for (i = 0; i < num; ++i)//ボールの数だけループ
+            {
+                balls[i].EraseCircle();//ボール消去
+            }
+            mainCanvas.Dispose();
+            mainCanvas = new Bitmap(mainPictureBox.Width, mainPictureBox.Height);
+            ini_Main_Balls(0, num_gold, num_colors);//金のボール初期化
+            ini_Main_Balls(num_gold, num, 0);//ボール初期化
+            timer1.Stop();//タイマーストップ
+            restartButton.Text = "START";
+            game_flag = false;
+            label2.Text = Ball.ballcount.ToString();//金のボールの数を表示
+        }
+        //クリックの当たり判定とゲーム終了処理
+        private void mainPictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            int i;
+            if (game_flag)//ゲーム中であれば
+            {
+                for (i = 0; i < num; ++i)//ボールの数だけループ
+                {
+                    balls[i].Check_Clicked(e.X, e.Y);//クリックの当たり判定
+                }
+                label2.Text = Ball.ballcount.ToString();//金のボールの数を表示
+                if(Ball.ballcount <= 0)//金のボール数が０になれば
+                {
+                    MessageBox.Show("ゲームクリア");//ゲームクリア
+                    reset_Game();//ゲーム終了、リセット
+                }
+            }
+        }
+        //タイマー作動時10msecごとに呼び出される、ボールの移動、衝突処理
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int i,m;
+            //全てのボール同士で衝突処理
+            for (i = 0; i < num; ++i)//ボールの数だけループ
+            {
+                for (m = i; m < num; ++m)//相手のボールの数だけループ
+                {
+                    balls[i].ColBall(balls[m]);//衝突処理
+                }
+                mainPictureBox.Image = balls[i].Move();//ボール移動
+            }
+        }
+        //ボール生成
+        private void make_Main_Balls(int start_index,int end_index,int colornumber)
+        {
+            int x, y, v, diam;
+            Brush color;
+            int i;
+            for (i = start_index ; i < end_index; ++i)
+            {
+                diam = rnd.Next(ballsize_min, ballseize_max);//ボールの直径をランダムに設定
+                x = rnd.Next(0, mainPictureBox.Width);       // ボールの初期位置をランダムに配置
+                if (x > mainPictureBox.Width - diam) { x = mainPictureBox.Width - diam; }//ゲーム画面外であればゲーム画面内に配置
+                y = rnd.Next(0, mainPictureBox.Height);      //ボールの初期位置をランダムに配置
+                if (y > mainPictureBox.Height - diam) { y = mainPictureBox.Height - diam; }//ゲーム画面外であればゲーム画面内に配置
+                v = rnd.Next(v_min, v_max);//ボールの速度をランダムに設定
+                color = colors[rnd.Next(num_colors)];//ボールの色をランダムに設定
+                if (colornumber == index_gold) { color = colors[num_colors]; }//ゴールドナンバーだったら色をゴールに設定
+                balls[i] = new Ball(diam, x, y, v, mainCanvas, color);//ボール生成
+                mainPictureBox.Image = balls[i].DrawCircle();//ボール描画
+            }
+        }
+        //ボール初期化、ボール生成とほぼ同じ処理、今あるボールを再設定
+        private void ini_Main_Balls(int start_index,int end_index,int colornumber)
+        {
+            int x, y, v, diam;
+            Brush color;
+            int i;
+            for (i = start_index; i < end_index; ++i)
+            {
+                diam = rnd.Next(ballsize_min, ballseize_max);
+                x = rnd.Next(0, mainPictureBox.Width);         
+                if (x > mainPictureBox.Width - diam) { x = mainPictureBox.Width - diam; }
+                y = rnd.Next(0, mainPictureBox.Height);        
+                if (y > mainPictureBox.Height - diam) { y = mainPictureBox.Height - diam; }
+                v = rnd.Next(v_min, v_max);
+                color = colors[rnd.Next(num_colors)];
+                if (colornumber == index_gold) { color = colors[num_colors]; }
+                balls[i].Ini(diam, x, y, v, mainCanvas, color);
+                mainPictureBox.Image = balls[i].DrawCircle();
+            }
+        }
+        
+    }
+
+    
+
+}
